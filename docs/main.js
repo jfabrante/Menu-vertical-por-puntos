343209b143e86e224b8cbf0369eec14738513346
Jose Francisco Padilla Abrante <80043180+jfabrante@users.noreply.github.com>
2021-07-18 11:31:14 +0200

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+document.querySelectorAll('#temario .lista a').forEach((elemento) => {
+    elemento.addEventListener('click', () => {
+        document.querySelector('#temario .activo').classList.remove('activo');
+        elemento.parentElement.classList.add('activo');
+    })
+});
